40b650c8896720040392b18e06d1a0606a5217f4
headcruser <headcruser@gmail.com>
2017-12-29 11:10:07 -0600

    Actualizacion de rutas de recursos

--- system.class.php
+++ system.class.php
@@ -1,18 +1,33 @@
 <?php
 	define('IMC', true);
 	
+	///////////////////// CONST SMARTY///////////////////////////////////
+
+	define ('CACHE', 'cache'.DIRECTORY_SEPARATOR);
+	
+	define ('CONFIGS', 'configs'.DIRECTORY_SEPARATOR);
+
+	define ('TEMPLATES_V', 'templates'.DIRECTORY_SEPARATOR);
+	
+	define ('TEMPLATES_C', 'templates_c'.DIRECTORY_SEPARATOR);
+
+	////////////////////// PATH SYSTEM /////////////////////////////////
+
 	define ("PATH",dirname(realpath(__FILE__)).DIRECTORY_SEPARATOR);
 
 	define ('INDEX', dirname($_SERVER['PHP_SELF']).DIRECTORY_SEPARATOR);
+
+	//////////////////////// RESOURCES /////////////////////////////////
 	
-	define ('CSS', INDEX.'css'.DIRECTORY_SEPARATOR);
+	define ('CSS', INDEX.'css'.DIRECTORY_SEPARATOR);	
 	
 	define ('JS', INDEX.'js'.DIRECTORY_SEPARATOR);
 	
 	define ('IMG', INDEX.'images'.DIRECTORY_SEPARATOR);
-	
+
 	define ('TEMPLATES', INDEX.'templates'.DIRECTORY_SEPARATOR);
 
+	//////////////////// COMPOSER DEPENDENCES /////////////////////////
 	define ('CSS_VENDOR', INDEX.'vendor\twbs\bootstrap\dist\css'.DIRECTORY_SEPARATOR);	
 	
 	define ('JS_VENDOR_BOOT', INDEX.'vendor\twbs\bootstrap\dist\js'.DIRECTORY_SEPARATOR);
@@ -34,10 +49,11 @@
 		private  function createView()
 		{
 			$this->view = new Smarty();
-			$this->view->setTemplateDir('templates/');
-			$this->view->setCompileDir('templates_c/');
-			$this->view->setConfigDir('configs/');
-			$this->view->setCacheDir('cache/');
+			$this->view->setTemplateDir( TEMPLATES_V );
+			$this->view->setCompileDir( TEMPLATES_C );
+			$this->view->setConfigDir( CONFIGS );
+			 $this->view->setCacheDir( CACHE );
+
 		}
 	}
 
